6ef6b79b05f914957f193219f310b9eecf2e5c91
Michele Wiseman <33985124+mswiseman@users.noreply.github.com>
2025-08-26 13:05:31 -0700

    Update jobs.sh

--- inference/jobs.sh
+++ inference/jobs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-time python inference_testing.py                \
---model_versions 210                            \
---output_dir /output/                           \
---confidence_threshold 0.5                      \
---csv_file_name example.csv                     \ # output csv
---model_path /models/detection_v210_3_class.pt  \
---ground_truth                                  \ 
---base_dir /data/modelv210/test/images/         \
---gt_label_dir /data/modelv210/test/labels/     \
+time python inference_testing.py                   \
+--model_versions 209                               \
+--output_dir ../test/output                        \
+--confidence_threshold 0.5                         \
+--csv_file_name example.csv                        \ # output csv
+--model_path ../models/detection_v209_5_class.pt   \
+--ground_truth                                     \ 
+--base_dir ../test/images/                         \
+--gt_label_dir ../test/labels/                     \
 --heatmap_performance
